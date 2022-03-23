fa5fc15d2af5f1ae784635af2a9fce68e12c6225
Stefan Krenz <sk@marmalade.group>
2022-03-23 14:09:39 +0100

    fix: Add comments

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -1,12 +1,25 @@
 <?php
+/**
+ * This file is part of Makaira.
+ *
+ * @see https://makaira.io
+ */
 
 namespace Makaira\OxidConnectEssential\Test;
 
 use Makaira\OxidConnectEssential\Dummy;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Dummy test case to set up sonarcloud.
+ */
 class DummyTest extends TestCase
 {
+    /**
+     * Dummy test to set up sonarcloud.
+     *
+     * @return void
+     */
     public function testDummyTest(): void
     {
         $o      = new Dummy();
