844935a5fd0c3e8362fe85ef80508d23bf3d7052
Stefan Krenz <sk@marmalade.group>
2022-03-23 10:46:51 +0100

    feat: Add basic module structure

---
+++ metadata.php
@@ -0,0 +1,12 @@
+<?php
+
+$sMetadataVersion = '2.1';
+
+$aModule          = [
+    'id'          => 'makaira_oxid-connect-essential',
+    'description' => '',
+    'thumbnail'   => 'makaira.jpg',
+    'version'     => '0.0.1',
+    'author'      => 'Makaira GmbH',
+    'controllers' => [],
+];
