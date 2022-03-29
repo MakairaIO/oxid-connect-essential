165ed93350bddaafff80c2aed6db250cebf386e0
Sunny <dt@marmalade.group>
2022-03-29 11:34:49 +0700

    fix: restore Dummy.php

---
+++ src/Dummy.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of Makaira.
+ *
+ * @see https://makaira.io
+ */
+
+namespace Makaira\OxidConnectEssential;
+
+/**
+ * Dummy class to set up sonarcloud.
+ */
+class Dummy
+{
+    /**
+     * Dummy method to set up sonarcloud.
+     *
+     * @param int $x
+     * @param int $y
+     *
+     * @return int
+     */
+    public function add(int $x, int $y): int
+    {
+        return $x + $y;
+    }
+}
