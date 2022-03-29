596aaa682b03f830f6ee9aa8a8d219ebe8deee08
Sunny <dt@marmalade.group>
2022-03-29 09:06:05 +0700

    MAKAIRA-2551 OXID essential: Reviews API

---
+++ Controller/ReviewController.php
@@ -0,0 +1,11 @@
+<?php
+
+use OxidEsales\Eshop\Application\Controller\FrontendController;
+
+class ReviewController extends FrontendController
+{
+    public function getReviews(): array
+    {
+        return [];
+    }
+}
