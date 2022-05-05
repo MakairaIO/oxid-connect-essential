0d3e0b31dc537b55fb39d70bd6e0e53a083d23ee
Stefan Krenz <sk@marmalade.group>
2022-05-05 15:09:24 +0200

    fix: Remove unnecessary check

--- src/Modifier/Common/StockModifier.php
+++ src/Modifier/Common/StockModifier.php
@@ -12,22 +12,14 @@ use Makaira\OxidConnectEssential\Type\Product\Product;
 
 class StockModifier extends Modifier
 {
-    private Connection $database;
-
-    private string $tableName;
-
     private bool $useStock;
 
     /**
-     * @param Connection $database
-     * @param string     $tableName
-     * @param bool       $useStock
+     * @param bool $useStock
      */
-    public function __construct(Connection $database, string $tableName, bool $useStock)
+    public function __construct(bool $useStock)
     {
-        $this->useStock  = $useStock;
-        $this->tableName = $tableName;
-        $this->database  = $database;
+        $this->useStock = $useStock;
     }
 
     /**
@@ -36,32 +28,12 @@ class StockModifier extends Modifier
      * @param Product $type
      *
      * @return Type
-     * @throws DBALDriverException
-     * @throws DBALException
      */
     public function apply(Type $type)
     {
-        $stockFlag = 1;
-        $stock     = 1;
-        $onStock   = true;
-
         if ($this->useStock) {
-            if (!isset($type->OXSTOCKFLAG, $type->OXSTOCK, $type->OXVARSTOCK)) {
-                $sql  = "SELECT OXPARENTID, OXSTOCKFLAG, OXSTOCK, OXVARSTOCK FROM {$this->tableName} WHERE OXID = ?";
-
-                /** @var Result $resultStatement */
-                $resultStatement = $this->database->executeQuery($sql, [$type->id]);
-
-                /** @var array<string, string> $result */
-                $result          = $resultStatement->fetchAssociative();
-                if ($result) {
-                    $stockFlag = (int) $result['OXSTOCKFLAG'];
-                    $stock     = (int) $result['OXSTOCK'] + (int) $result['OXVARSTOCK'];
-                }
-            } else {
-                $stockFlag = (int) $type->OXSTOCKFLAG;
-                $stock     = (int) ($type->OXSTOCK + $type->OXVARSTOCK);
-            }
+            $stockFlag = (int) $type->additionalData['OXSTOCKFLAG'];
+            $stock     = (int) $type->additionalData['OXSTOCK'] + (int) $type->additionalData['OXVARSTOCK'];
 
             // 1 --> Standard
             // 2 --> Wenn ausverkauft offline
@@ -71,10 +43,10 @@ class StockModifier extends Modifier
             if (4 === $stockFlag) {
                 $stock = 1;
             }
-        }
 
-        $type->onstock = $onStock;
-        $type->stock   = $stock;
+            $type->onstock = $onStock;
+            $type->stock   = $stock;
+        }
 
         return $type;
     }
