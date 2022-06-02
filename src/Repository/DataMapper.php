11e2b89ba9b7ee2f2768942f74fe8049d60b9e16
Stefan Krenz <sk@marmalade.group>
2022-06-02 15:10:36 +0200

    Refactor data mapper

--- src/Repository/DataMapper.php
+++ src/Repository/DataMapper.php
@@ -12,11 +12,7 @@ use ReflectionClass;
 use ReflectionNamedType;
 use ReflectionProperty;
 
-use function array_change_key_case;
-use function array_merge;
 use function get_class;
-
-use const CASE_UPPER;
 
 class DataMapper
 {
@@ -55,39 +51,53 @@ class DataMapper
         'OXTITLE'     => 'manufacturer_title',
     ];
 
+    /**
+     * @var array<string, array<string, string>>
+     */
+    private static ?array $fieldMappings = null;
+
+    /**
+     * @var array<array<string, Closure>>
+     */
     private static array $dataTypes = [];
 
     /**
-     * @param Category|Product|Manufacturer|Variant $entity
-     * @param array                                 $dbResult
-     * @param string                                $docType
+     *
+     */
+    public function __construct()
+    {
+        if (null === self::$fieldMappings) {
+            self::$fieldMappings[Product::class] = array_replace(
+                self::COMMON_FIELD_MAPPING,
+                self::PRODUCT_FIELD_MAPPING
+            );
+
+            self::$fieldMappings[Variant::class] = array_replace(
+                self::COMMON_FIELD_MAPPING,
+                self::PRODUCT_FIELD_MAPPING
+            );
+
+            self::$fieldMappings[Category::class] = array_replace(
+                self::COMMON_FIELD_MAPPING,
+                self::CATEGORY_FIELD_MAPPING
+            );
+
+            self::$fieldMappings[Manufacturer::class] = array_replace(
+                self::COMMON_FIELD_MAPPING,
+                self::MANUFACTURER_FIELD_MAPPING
+            );
+        }
+    }
+
+    /**
+     * @param Type  $entity
+     * @param array $dbResult
      *
      * @return void
-     * @SuppressWarnings(CyclomaticComplexity)
      */
-    public function map(Type $entity, array $dbResult, string $docType): void
+    public function map(Type $entity, array $dbResult): void
     {
-        $mappingFields = [];
-
-        switch ($docType) {
-            case "product":
-            case "variant":
-                $mappingFields = self::PRODUCT_FIELD_MAPPING;
-                break;
-
-            case "category":
-                $mappingFields = self::CATEGORY_FIELD_MAPPING;
-                break;
-
-            case "manufacturer":
-                $mappingFields = self::MANUFACTURER_FIELD_MAPPING;
-                break;
-
-            default:
-                break;
-        }
-
-        $mappingFields = array_merge(self::COMMON_FIELD_MAPPING, $mappingFields);
+        $mappingFields = self::$fieldMappings[get_class($entity)] ?? self::COMMON_FIELD_MAPPING;
 
         $fieldDataTypes = $this->getFieldDataTypes($entity);
 
