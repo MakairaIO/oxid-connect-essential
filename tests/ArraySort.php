fd80c4d84ef75011e35014c598933069a3fe9db4
Stefan Krenz <krenz.stefan@googlemail.com>
2023-03-09 16:28:19 +0100

    Fix sorting algorithm

--- tests/ArraySort.php
+++ tests/ArraySort.php
@@ -2,60 +2,69 @@
 
 namespace Makaira\OxidConnectEssential\Test;
 
+use function array_reverse;
 use function count;
 
-trait ArraySortTrait
+class ArraySort
 {
-    protected function mergeSort(array $array, array $sorting = [0 => 'asc']): array
+    public const ASCENDING = 0;
+
+    public const DESCENDING = 1;
+
+    public static function mergeSort(array $array, array $sorting = [0 => self::ASCENDING]): array
     {
+        $sorted = $array;
+        $self = new static();
+        $sorting = array_reverse($sorting, true);
+
         foreach ($sorting as $key => $direction) {
-            $array = $this->doMergeSort($array, $key, $direction);
+            $sorted = $self->doMergeSort($sorted, $key, $direction === self::ASCENDING);
         }
 
-        return $array;
+        return $sorted;
     }
 
-    private function doMergeSort(array $array, string|int $key, string $direction = 'asc'): array
+    private function doMergeSort(array $array, string|int $key, bool $asc): array
     {
-        if (count($array) == 1) {
+        if (count($array) <= 1) {
             return $array;
         }
 
         $mid   = count($array) / 2;
         $left  = array_slice($array, 0, $mid);
         $right = array_slice($array, $mid);
-        $left  = $this->doMergeSort($left, $key, $direction);
-        $right = $this->doMergeSort($right, $key, $direction);
+        $left  = $this->doMergeSort($left, $key, $asc);
+        $right = $this->doMergeSort($right, $key, $asc);
 
-        return $this->merge($left, $right, $key, $direction);
+        return $this->merge($left, $right, $key, $asc);
     }
 
-    private function merge(array $left, array $right, string|int $key, string $direction = 'asc'): array
+    private function merge(array $left, array $right, string|int $key, bool $asc): array
     {
         $res = [];
 
         $cmp = static fn ($arrayA, $arrayB) => strcmp(
-            $direction === 'asc' ? $arrayA[$key] : $arrayB[$key],
-            $direction === 'asc' ? $arrayB[$key] : $arrayA[$key],
+            $asc ? $arrayA[$key] : $arrayB[$key],
+            $asc ? $arrayB[$key] : $arrayA[$key],
         );
 
         while (count($left) > 0 && count($right) > 0) {
-            if ($cmp($left, $right) > 0) {
-                $res[] = $right[$key];
+            if ($cmp($left[0], $right[0]) > 0) {
+                $res[] = $right[0];
                 $right = array_slice($right, 1);
             } else {
-                $res[] = $left[$key];
+                $res[] = $left[0];
                 $left  = array_slice($left, 1);
             }
         }
 
         while (count($left) > 0) {
-            $res[] = $left[$key];
+            $res[] = $left[0];
             $left  = array_slice($left, 1);
         }
 
         while (count($right) > 0) {
-            $res[] = $right[$key];
+            $res[] = $right[0];
             $right = array_slice($right, 1);
         }
 
