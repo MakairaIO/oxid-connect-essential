63ef3bd30463d6783ae3eb5270a9866c2df3052b
Stefan Krenz <krenz.stefan@googlemail.com>
2023-03-10 12:13:56 +0100

    fix(Modifier): Use 01.01.0001 as zero date

--- src/Modifier/Common/TimestampNormalizer.php
+++ src/Modifier/Common/TimestampNormalizer.php
@@ -9,10 +9,20 @@ use Makaira\OxidConnectEssential\Type;
 
 class TimestampNormalizer extends Modifier
 {
+    private DateTimeInterface $zeroDate;
+
+    public function __construct()
+    {
+        $this->zeroDate = new DateTimeImmutable('0001-01-01T00:00:00');
+    }
+
     public function apply(Type $type)
     {
         if ($type->timestamp) {
             $type->timestamp = (new DateTimeImmutable($type->timestamp))->format('Y-m-d H:i:s');
+            if ($type->timestamp < $this->zeroDate) {
+                $type->timestamp = $this->zeroDate;
+            }
         }
 
         return $type;
