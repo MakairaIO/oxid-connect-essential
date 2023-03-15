46b74c45af663b5a084196f0be847eda8407b8d6
Stefan Krenz <krenz.stefan@googlemail.com>
2023-03-15 14:36:29 +0100

    Fix tests

--- tests/Integration/Modifier/Product/ActiveModifierTest.php
+++ tests/Integration/Modifier/Product/ActiveModifierTest.php
@@ -5,6 +5,7 @@ namespace Makaira\OxidConnectEssential\Test\Integration\Modifier\Product;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver\Result;
 use Makaira\OxidConnectEssential\Modifier\Product\ActiveModifier;
+use Makaira\OxidConnectEssential\Test\TableTranslatorTrait;
 use Makaira\OxidConnectEssential\Type\Product\Product as ProductType;
 use OxidEsales\Eshop\Application\Model\Category;
 use OxidEsales\Eshop\Core\Registry as EshopRegistry;
@@ -13,6 +14,8 @@ use PHPUnit\Framework\TestCase;
 
 class ActiveModifierTest extends TestCase
 {
+    use TableTranslatorTrait;
+
     public function testModifyTypeIfActive(): void
     {
         $resultMock = $this->createMock(Result::class);
@@ -20,8 +23,8 @@ class ActiveModifierTest extends TestCase
             ->willReturn('1');
 
         $sql = "SELECT COUNT(OXID) > 0
-            FROM oxcategories
-            WHERE OXID = '42' AND (  oxcategories.oxactive = 1  and  oxcategories.oxhidden = '0'  ) ";
+            FROM oxv_oxcategories_de
+            WHERE OXID = '42' AND (  oxv_oxcategories_de.oxactive = 1  and  oxv_oxcategories_de.oxhidden = '0'  ) ";
 
         $databaseMock = $this->createMock(Connection::class);
         $databaseMock->method('executeQuery')
@@ -37,22 +40,27 @@ class ActiveModifierTest extends TestCase
 
         UtilsObject::setClassInstance(Category::class, $modelMock);
 
-        $modifier = new ActiveModifier($databaseMock, Category::class, EshopRegistry::getUtilsObject());
+        $modifier = new ActiveModifier(
+            $databaseMock,
+            Category::class,
+            EshopRegistry::getUtilsObject(),
+            $this->getTableTranslatorMock()
+        );
         $type = new ProductType(['id' => 42, 'active' => false]);
         $currentType = $modifier->apply($type);
 
         static::assertTrue($currentType->active);
     }
 
-    public function testModifyTypeIfInctive(): void
+    public function testModifyTypeIfInactive(): void
     {
         $resultMock = $this->createMock(Result::class);
         $resultMock->method('fetchOne')
             ->willReturn('0');
 
         $sql = "SELECT COUNT(OXID) > 0
-            FROM oxcategories
-            WHERE OXID = '42' AND (  oxcategories.oxactive = 1  and  oxcategories.oxhidden = '0'  ) ";
+            FROM oxv_oxcategories_de
+            WHERE OXID = '42' AND (  oxv_oxcategories_de.oxactive = 1  and  oxv_oxcategories_de.oxhidden = '0'  ) ";
 
         $databaseMock = $this->createMock(Connection::class);
         $databaseMock->method('executeQuery')
@@ -68,7 +76,12 @@ class ActiveModifierTest extends TestCase
 
         UtilsObject::setClassInstance(Category::class, $modelMock);
 
-        $modifier = new ActiveModifier($databaseMock, Category::class, EshopRegistry::getUtilsObject());
+        $modifier = new ActiveModifier(
+            $databaseMock,
+            Category::class,
+            EshopRegistry::getUtilsObject(),
+            $this->getTableTranslatorMock()
+        );
         $type = new ProductType(['id' => 42, 'active' => true]);
         $currentType = $modifier->apply($type);
 
