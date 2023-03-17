832aaec64f896a7e523fcf2fdac2e328fcee9674
Stefan Krenz <krenz.stefan@googlemail.com>
2023-03-17 12:18:29 +0100

    Use OXID CE classes for phpstan

--- tests/PhpStan/phpstan-bootstrap.php
+++ tests/PhpStan/phpstan-bootstrap.php
@@ -10,46 +10,46 @@ declare(strict_types=1);
 include __DIR__ . '/../../vendor/autoload.php';
 
 class_alias(
-    OxidEsales\Eshop\Application\Controller\Admin\ArticleAttributeAjax::class,
+    OxidEsales\EshopCommunity\Application\Controller\Admin\ArticleAttributeAjax::class,
     Makaira\OxidConnectEssential\Controller\Admin\ArticleAttributeAjax_parent::class
 );
 
 class_alias(
-    OxidEsales\Eshop\Application\Controller\Admin\ArticleCrosssellingAjax::class,
+    OxidEsales\EshopCommunity\Application\Controller\Admin\ArticleCrosssellingAjax::class,
     Makaira\OxidConnectEssential\Controller\Admin\ArticleCrossSellingAjax_parent::class
 );
 
 class_alias(
-    OxidEsales\Eshop\Application\Controller\Admin\ArticleExtendAjax::class,
+    OxidEsales\EshopCommunity\Application\Controller\Admin\ArticleExtendAjax::class,
     Makaira\OxidConnectEssential\Controller\Admin\ArticleExtendAjax_parent::class
 );
 
 class_alias(
-    OxidEsales\Eshop\Application\Controller\Admin\ArticleSelectionAjax::class,
+    OxidEsales\EshopCommunity\Application\Controller\Admin\ArticleSelectionAjax::class,
     Makaira\OxidConnectEssential\Controller\Admin\ArticleSelectionAjax_parent::class
 );
 
 class_alias(
-    OxidEsales\Eshop\Application\Controller\Admin\AttributeMainAjax::class,
+    OxidEsales\EshopCommunity\Application\Controller\Admin\AttributeMainAjax::class,
     Makaira\OxidConnectEssential\Controller\Admin\AttributeMainAjax_parent::class
 );
 
 class_alias(
-    OxidEsales\Eshop\Application\Controller\Admin\CategoryOrderAjax::class,
+    OxidEsales\EshopCommunity\Application\Controller\Admin\CategoryOrderAjax::class,
     Makaira\OxidConnectEssential\Controller\Admin\CategoryOrderAjax_parent::class
 );
 
 class_alias(
-    OxidEsales\Eshop\Application\Controller\Admin\ManufacturerMainAjax::class,
+    OxidEsales\EshopCommunity\Application\Controller\Admin\ManufacturerMainAjax::class,
     Makaira\OxidConnectEssential\Controller\Admin\ManufacturerMainAjax_parent::class
 );
 
 class_alias(
-    OxidEsales\Eshop\Application\Controller\Admin\SelectListMainAjax::class,
+    OxidEsales\EshopCommunity\Application\Controller\Admin\SelectListMainAjax::class,
     Makaira\OxidConnectEssential\Controller\Admin\SelectListMainAjax_parent::class
 );
 
 class_alias(
-    OxidEsales\Eshop\Core\Output::class,
+    OxidEsales\EshopCommunity\Core\Output::class,
     Makaira\OxidConnectEssential\Oxid\Core\MakairaConnectOutput_parent::class
 );
